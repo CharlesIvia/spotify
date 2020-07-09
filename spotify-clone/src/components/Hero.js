c465f24fc4ee6662bf328c99c1d144c9f2f15cb4
Charles Ivia <iviacharles@gmail.com>
2020-07-09 15:58:10 +0300

    Created and added Header component

--- spotify-clone/src/components/Hero.js
+++ spotify-clone/src/components/Hero.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Buttons from "./Buttons";
+import Header from "./Header";
 
 function Hero() {
   return (
     <div>
+      <Header />
       <div className="title">
         <h1>Get 3 months of Premium for $9.99</h1>
       </div>
