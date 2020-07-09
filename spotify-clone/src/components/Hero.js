90b011b05b802bdaabb0762f52583618f4f3b05f
Charles Ivia <iviacharles@gmail.com>
2020-07-09 14:47:40 +0300

    Created and intergrated Hero component

---
+++ spotify-clone/src/components/Hero.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div>
+      <div className="title">
+        <h1>Get 3 months of Premium for $9.99</h1>
+      </div>
+
+      <div>
+        <h2>Enjoy endless ad-free music - even offline.</h2>
+      </div>
+
+      <div>
+        <small>
+          *Monthly subscription fee aplies after. Offer ends Jul 30, 2020.
+        </small>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
