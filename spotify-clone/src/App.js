b647e72a4c4b9d9fadb402fba17e372ea19361fe
Charles Ivia <iviacharles@gmail.com>
2020-07-09 15:16:47 +0300

    Created and intergrated Cancel.js component

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Hero from "./components/Hero";
 import Premium from "./components/Premium";
+import Cancel from "./components/Cancel";
 
 function App() {
   return (
     <div className="App">
       <Hero />
       <Premium />
+      <Cancel />
     </div>
   );
 }
---
+++ spotify-clone/src/components/Cancel.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Cancel() {
+  return (
+    <div>
+      <div className="switch">
+        <svg
+          width="24"
+          height="24"
+          xmlns="http://www.w3.org/2000/svg"
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+        >
+          <path d="M6 18h12c3.311 0 6-2.689 6-6s-2.689-6-6-6h-12.039c-3.293.021-5.961 2.701-5.961 6 0 3.311 2.688 6 6 6zm12-10c-2.208 0-4 1.792-4 4s1.792 4 4 4 4-1.792 4-4-1.792-4-4-4z" />
+        </svg>
+      </div>
+
+      <div className="instructions">
+        <h1>Cancel anytime</h1>
+        <small>You can cancel monthly plans online anytime.</small>
+      </div>
+    </div>
+  );
+}
+
+export default Cancel;
