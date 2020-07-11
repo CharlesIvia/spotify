148c235f3808bb7097f8df5b2ddebac4a30d5abb
Charles Ivia <iviacharles@gmail.com>
2020-07-11 14:29:49 +0300

    Updates

---
+++ precache-manifest.a110362bca1363e3ce7ac836b75d2fcf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18be41d6bae6334b35257c9f3298ec10",
+    "url": "/spotify/index.html"
+  },
+  {
+    "revision": "05c00cdbcd017c4f5d4e",
+    "url": "/spotify/static/css/main.cc59f7c5.chunk.css"
+  },
+  {
+    "revision": "b8990bdabf12f717c2b7",
+    "url": "/spotify/static/js/2.de67c717.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/spotify/static/js/2.de67c717.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05c00cdbcd017c4f5d4e",
+    "url": "/spotify/static/js/main.dcae95a3.chunk.js"
+  },
+  {
+    "revision": "b5716cca5026fcf1a80e",
+    "url": "/spotify/static/js/runtime-main.41e95489.js"
+  }
+]);
